4fda20eb3798d2adb239e60f34c207ba6b3e3ae3
Chris Keller <xylo04@gmail.com>
2020-12-07 15:15:42 -0700

    When setting sliceflag, clear defaults

--- sliceflag.go
+++ sliceflag.go
@@ -10,6 +10,7 @@ func (i *sliceFlag) String() string {
 
 func (i *sliceFlag) Set(value string) error {
 	tokens := strings.Split(value, ",")
+	*i = make([]string, 0)
 	for _, t := range tokens {
 		*i = append(*i, t)
 	}
