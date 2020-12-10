4fa1b9b780cf131651f2953ff63c01b6e0442763
Chris Keller <xylo04@gmail.com>
2020-12-09 21:41:29 -0700

    Maybe egrep will be more portable

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 
-VERSION=${VERSION:-$(< debian/changelog head -1 | sed -r 's/.*\(([0-9]+\.[0-9]+\.[0-9]+)-.*\).*/v\1/g')}
+VERSION=${VERSION:-v$(< debian/changelog head -1 | egrep -o "[0-9]+\.[0-9]+\.[0-9]+")}
 GITCOMMIT=${GITCOMMIT:-$(git rev-parse --short HEAD 2> /dev/null || true)}
 BUILDTIME=${BUILDTIME:-$(date -u +"%Y-%m-%dT%H:%M:%SZ")}
 
