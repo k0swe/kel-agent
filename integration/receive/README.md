c19c3ad340e82d6a6d456b96513813c5d3a2a676
Chris Keller <xylo04@gmail.com>
2021-11-19 22:39:55 -0700

    Generalize receive integration test

---
+++ integration/receive/README.md
@@ -0,0 +1,2 @@
+When the fake WSJTX server emits the `.bin` file to kel-agent, kel-agent should send the
+corresponding `.json` data to the websocket client.
--- integration/receive_test.go
+++ integration/receive_test.go
@@ -2,19 +2,27 @@ package integration
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"testing"
 
 	"github.com/k0swe/kel-agent/internal/server"
 )
 
-func (s *integrationTestSuite) TestReceiveHeartbeat() {
-	input, _ := ioutil.ReadFile("receive/heartbeat.bin")
-	want, _ := ioutil.ReadFile("receive/heartbeat.json")
-	_, _ = s.fake.SendMessage(input)
+func (s *integrationTestSuite) TestReceive() {
+	tests := []string{"heartbeat"}
 
-	_, got, err := s.wsClient.ReadMessage()
-	s.Require().NoError(err)
-	wantObj := json.Unmarshal(want, &server.WebsocketMessage{})
-	gotObj := json.Unmarshal(got, &server.WebsocketMessage{})
-	s.Require().Equal(wantObj, gotObj)
+	for _, tt := range tests {
+		s.T().Run(tt, func(t *testing.T) {
+			input, _ := ioutil.ReadFile(fmt.Sprintf("receive/%s.bin", tt))
+			want, _ := ioutil.ReadFile(fmt.Sprintf("receive/%s.json", tt))
+			_, _ = s.fake.SendMessage(input)
+
+			_, got, err := s.wsClient.ReadMessage()
+			s.Require().NoError(err)
+			wantObj := json.Unmarshal(want, &server.WebsocketMessage{})
+			gotObj := json.Unmarshal(got, &server.WebsocketMessage{})
+			s.Require().Equal(wantObj, gotObj)
+		})
+	}
 }
