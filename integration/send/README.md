566c794e30ca07d94abec732835d55b3fd34c183
Chris Keller <xylo04@gmail.com>
2021-11-20 09:13:09 -0700

    Add sending integration tests

---
+++ integration/send/README.md
@@ -0,0 +1,2 @@
+When the websocket client sends the `.json` file to kel-agent, kel-agent should send the
+corresponding `.bin` data to the fake WSJTX server.
---
+++ integration/send_test.go
@@ -0,0 +1,47 @@
+package integration
+
+import (
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"testing"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+func (s *integrationTestSuite) TestSend() {
+	s.primeConnection()
+	tests := []string{
+		"clear",
+		"close",
+	}
+
+	for _, tt := range tests {
+		s.T().Run(tt, func(t *testing.T) {
+			input, _ := ioutil.ReadFile(fmt.Sprintf("send/%s.json", tt))
+			want, _ := ioutil.ReadFile(fmt.Sprintf("send/%s.bin", tt))
+			err := s.wsClient.WriteMessage(websocket.TextMessage, input)
+			s.Require().NoError(err)
+
+			select {
+			case got := <-s.fake.ReceiveChan:
+				s.Require().NoError(err)
+				s.Require().Equal(want, got)
+			case <-time.After(500 * time.Second):
+				t.Log("timeout")
+				t.Fail()
+			}
+		})
+	}
+}
+
+func (s *integrationTestSuite) primeConnection() {
+	// Because this is UDP, the server doesn't have an address for WSJTX until WSJTX has sent the
+	// server a message.
+	clearMsg, _ := hex.DecodeString(`adbccbda00000002000000030000000657534a542d58`)
+	_, err := s.fake.SendMessage(clearMsg)
+	s.Require().NoError(err)
+	_, _ = s.fake.SendMessage(clearMsg)
+	s.T().Log("connection is primed for a send test")
+}
